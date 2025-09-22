821971f175d647888f38ccf61be53a5c1d70b4dc
Anton Lee <anton.j.15.le@gmail.com>
2025-09-22 17:18:34 +1200

    update with `bayescl/SplitDomainNet/ball/34ed8bb`

--- jobs.py
+++ jobs.py
@@ -10,15 +10,15 @@ METHODS = [
     "01_linear",
     "02_lora",
     "03_ball",
-    "04_replay",
-    "05_gdumb",
-    "06_der",
+    # "04_replay",
+    # "05_gdumb",
+    # "06_der",
     "07_joint",
     "08_rwalk",
 ]
 
 # for dataset, method in product(DATASETS, METHODS):
-for method, dataset in product(METHODS, DATASETS):
+for dataset, method in product(DATASETS, METHODS):
     label = f"{dataset[:5]}_{method[3 : 5 + 3]}"
     print(
         f"ts -G 1 -L {label:<11} python main.py --hpsearch -c configs/{dataset}/{method}.yaml"
