07654572b15d7cd6cee94a28e8485cced085309d
Anton Lee <anton.lee@vuw.ac.nz>
2026-03-12 12:13:46 +1300

    add docs

--- bayescl/methods/clora/_config.py
+++ bayescl/methods/clora/_config.py
@@ -6,6 +6,8 @@ from bayescl.base import BaseConfig
 class CLoRAConfig(BaseConfig):
     type: Literal["CLoRA"] = "CLoRA"
     rank: int
+    """Rank of LoRA adapters."""
     alpha: float
+    """LoRA scaling factor."""
     lambda_: float
     """How much to penalize changes compared to the anchors."""
